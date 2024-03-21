e2d3971ec29363e404615e12b98c5fb6ef6b22c5
michelearcopinto <guestphantom1@gmail.com>
2024-03-21 17:56:54 +0100

    feat: database seeded

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $movies = config('movies');
+
+        foreach ($movies as $element) {
+            Movie::create([
+                'title' => $element['title'],
+                'director' => $element['director'],
+                'year' => $element['year'],
+                'genre' => $element['genre'],
+                'rating' => $element['rating'],
+            ]);
+        }
     }
 }
